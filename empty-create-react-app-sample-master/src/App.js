2f39dfba42ad7d7388a16356e2ca17c6fd26e817
chevery <cheobia@icloud.com>
2020-11-01 18:47:13 +0100

    just updating

--- empty-create-react-app-sample-master/src/App.js
+++ empty-create-react-app-sample-master/src/App.js
@@ -1,18 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+//
+// let width = 500,
+//     height = 0,
+//     filter = 'none',
+//     streaming = false;
+//
+// const video = document.getElementById('video');
+// const canvas = document.getElementById('canvas');
+// const photos = document.getElementById('photos');
+// const photoButton = document.getElementById('photo-button');
+// const clearButton = document.getElementById('clear-button');
+// const photoFilter = document.getElementById('photo-filter');
+//
+//
+//
+//     navigator.mediaDevices.getUserMedia({video: true, audio: true}
+//     )
+//         .then(function(stream){
+//         video.current.srcObject = stream;
+//         video.current.play();
+// })
+//         .catch(function (err) {
+//         console.log(`Error: ${err}`)
+//         })
+//     ;
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.localVideoref = React.createRef()
+    }
   render() {
-    return (
+      const constraints = {video: true}
+      const success = (stream) => {
+          this.localVideoref.current.srcObject = stream
+        }
+
+        const failure = (e) => {
+          console.log('getUserMedia Error:', e)
+        }
+
+
+          navigator.mediaDevices.getUserMedia(constraints, success, failure)
+
+      return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <video ref={this.localVideoref} autoPlay></video>
+          <button id="filter-button" className="btn btn-dark">
+              Choose filter
+          </button>
+          <select id="filter-id">
+              <option value="none">Normal</option>
+              <option value="none">Sepia</option>
+          </select>
+          <button id="clear-button">Clear</button>
+
       </div>
     );
   }
---
+++ empty-create-react-app-sample-master/src/videoComponent.js
@@ -0,0 +1,23 @@
+class Example extends Component {
+    constructor(props) {
+        super(props);
+        this.videoTag = React.createRef()
+    }
+
+    componentDidMount() {
+        // getting access to webcam
+        navigator.mediaDevices
+            .getUserMedia({video: true})
+            .then(stream => this.videoTag.current.srcObject = stream)
+            .catch(console.log);
+    }
+
+    render() {
+        return (
+            <video
+                ref={this.videoTag}
+                autoPlay
+            />
+        )
+    }
+}
